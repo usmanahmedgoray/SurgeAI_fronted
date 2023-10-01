fac4e56bf98ff00693a4f7a16c0bcb5fde5d3ecd
usmanahmedgoray <usmanahmedkharal@gmail.com>
2023-10-01 18:01:28 +0500

    Change URL

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
     const fetchPosts = async () =>{
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post',{
+        const response = await fetch('https://backend-surge-ai-ea020498fad1.herokuapp.com/api/v1/post',{
           method : 'GET',
           headers:{
             "Content-Type" : "application/json"
